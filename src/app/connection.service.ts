611558983248e07947984d074e7c2de202592554
ElMagis <multicuenta.andrada@gmail.com>
2025-11-10 14:39:35 -0300

    asd

--- src/app/connection.service.ts
+++ src/app/connection.service.ts
@@ -481,9 +481,11 @@ export class ConnectionService {
 
     async getEndlessGuesses(){
     try{
-      var response = await axios.get(this.url + this.endlessModeRouter + "getEndlessGuesses", this.getHeaders());
       console.log("HHHHHHHH")
+      var response = await axios.get(this.url + this.endlessModeRouter + "getEndlessGuesses", this.getHeaders());
+      console.log("IIIIIIIIIIIIIIIIIII")
       console.log(JSON.stringify(response.data))
+      console.log("JJJJJJJJJJJJJJJ")
       return response.data.generations;
     }
       catch (error) {
